36a5a619f4b573cb876f6734a86d6bc13b63e9a3
ge5rg2 <kghee9612@gmail.com>
2023-06-23 18:55:22 +0900

    feat: add search station api

--- routes/subway.js
+++ routes/subway.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const { subwayStation } = require("./subwayStation");
 require("dotenv").config();
 
 router.get("/:station", async (req, res, next) => {
   try {
     const { station = "장승배기" } = req.params;
-    const apiKey = process.env.OPEN_API;
+    const apiKey = process.env.SUBWAY_KEY;
     const url = `http://swopenAPI.seoul.go.kr/api/subway/${apiKey}/json/realtimeStationArrival/0/5/${station}`;
 
     const response = await axios.get(url);
     const data = response.data;
 
     res.send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error occurred");
+  }
+});
+
+router.get("/station/:line", async (req, res, next) => {
+  try {
+    const { line } = req.params;
+    // const jsonData = JSON.stringify(subwayStation);
+    const result = subwayStation.filter((el) => el.호선이름 == `${line}호선`);
+    console.log(result);
+    return;
   } catch (error) {
     console.error(error);
     res.status(500).send("Error occurred");
